0ab0582295e75e3f96e1c609bbb122b5501c0709
hws522 <sraphic@kakao.com>
2020-12-21 07:35:16 +0900

    mvc, templateEngine use

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +13,11 @@ public class HelloController {
         model.addAttribute("data", "spring!!");
         return "hello"; //여기서 hello 는 hello.html
     }
+
+    @GetMapping("hello-mvc")
+    public String helloNvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name); //파라미터로 넘어온 name을 넘겨줌.
+        return "hello-template";
+    }
+
 }
