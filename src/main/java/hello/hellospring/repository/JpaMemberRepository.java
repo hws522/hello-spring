b6034aab2cf09a43d2a36991e9bc1a408177b35f
hws522 <sraphic@kakao.com>
2020-12-25 07:44:28 +0900

    JPA add

---
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,46 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository{
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    // findByName, findAll 처럼 여러 리스트를 조회해야 하는 경우(PK 기반이 아닌 나머지) jpql을 작성해줘야함.
+    // jpa를 spring에 감싸서 제공하는 기술이 있음. -->> spring data jpa.
+    // 이걸 사용하면 jpql 작성이 필요 없어짐.
+}
--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -5,6 +5,7 @@ import hello.hellospring.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,6 +19,7 @@ class MemberServiceIntegrationTest {
     @Autowired  MemberRepository memberRepository;
 
     @Test
+
     void join() { //회원가입
         //given
         Member member = new Member();
